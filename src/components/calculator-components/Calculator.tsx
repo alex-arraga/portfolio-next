5f28fa1f8e5c61faf126205f811eb03adccc1d34
alex-arraga <alex-arraga@users.noreply.github.com>
2023-10-17 14:17:41 -0300

    Funtion calc added and running

--- src/components/calculator-components/Calculator.tsx
+++ src/components/calculator-components/Calculator.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { evaluate } from 'mathjs'
+
 import React from 'react'
 import { Button } from './Button'
 import { Screen } from './Screen'
@@ -7,14 +9,11 @@ import { Rows } from './Rows'
 import { Container } from './Container'
 import { FiDelete } from 'react-icons/fi';
 
-import imgRaiz from '@/images/calculator/raiz-cuadrada.svg'
-import imgPotencia from '@/images/calculator/potencia.svg'
-
 import { useEffect, useState } from 'react'
 
 export function Calculator() {
     const [valueScreen, setValueScreen] = useState('');
-    const [history, setHistory] = useState([]);
+    const [history, setHistory] = useState<Array<Record<string, any>>>([]);
     const [operationNumber, setOperationNumber] = useState(1);
     const [lastResult, setLastResult] = useState([]);
 
@@ -23,7 +22,6 @@ export function Calculator() {
     const emptyBrackets = /\(\)|[\(\)]/;
     const validExpression = /^[0-9+\-*/^()%x÷√!lnlog,]+$/.test(valueScreen);
     const comma = /[,]/;
-
 
     // Validations
     const lastCharacter = valueScreen.slice(-1);
@@ -59,6 +57,74 @@ export function Calculator() {
     };
 
 
+    // Almacena una lista de objetos como registros en Historial
+    const saveHistoy = (operationNumber: number, expression: string, result: number) => {
+        const newRegister = {
+            operationNumber,
+            expression,
+            result
+        }
+        // Recorre el array 'historial' y añade un nuevo registro al final
+        setHistory([...history, newRegister]);
+    };
+
+
+    // Evaluar resultados
+    const calc = () => {
+        if ((valueScreen || expressionInBrackets && validExpression && !hasEmptyBrackets))
+            if (!endsOperator && !endsComma) {
+                try {
+                    // Historial: Suma 1 a N°
+                    const nOperacion = () => {
+                        setOperationNumber(() => operationNumber + 1);
+                        return operationNumber
+                    };
+
+                    const resultado = evaluate(valueScreen);
+                    // const redondearDecimales = resultado.toFixed(1);
+                    // const resultadoString = redondearDecimales.toString();
+
+                    // No redondea decimales
+                    const resultadoString = resultado.toString();
+
+                    const resultadoFormateado = resultadoString.replace('.', ',');
+                    const eliminarCeroFinal = /,0$/.test(resultadoFormateado);
+                    const resFinal = resultadoFormateado;
+
+                    if (eliminarCeroFinal) {
+                        // Si termina en ',0' lo elimina y redondea
+                        const resFinalDecimal = resFinal.slice(0, -2);
+
+                        // Muestro el resultado de la expresion
+                        setValueScreen(resFinalDecimal);
+
+                        // Almacena el ultimo resultado (ANS)
+                        setLastResult(resFinalDecimal);
+                        // Pasa argumentos al objeto 'nuevoRegistro'
+                        saveHistoy(nOperacion(), valueScreen, resFinalDecimal);
+                    } else {
+                        // Muestro el resultado de la expresion
+                        setValueScreen(resFinal);
+
+                        // Almacena el ultimo resultado (ANS)
+                        setValueScreen(resFinal);
+                        // Pasa argumentos al objeto 'nuevoRegistro'
+                        saveHistoy(nOperacion(), valueScreen, resFinal);
+                    }
+                } catch {
+                    setValueScreen('Error');
+                    alert('Expresión no válida');
+                }
+            }
+            else {
+                setValueScreen('Error')
+            }
+        else {
+            alert('Ingrese una expresión')
+        }
+    };
+
+
     return (
         <main className='flex justify-center items-center h-screen'>
 
@@ -71,13 +137,13 @@ export function Calculator() {
                     <Button onClick={() => deleteAValue()}>
                         <FiDelete></FiDelete>
                     </Button>
-                    <Button onClick={() => getLastResult()}>AC</Button>
+                    <Button onClick={() => setValueScreen('')}>AC</Button>
                 </Rows>
                 <Rows>
                     <Button onClick={() => showValue('cos(')}>cos</Button>
                     <Button onClick={() => showValue('sin(')}>sin</Button>
                     <Button onClick={() => showValue('tan(')}>tan</Button>
-                    <Button onClick={() => showValue('-')}>ANS</Button>
+                    <Button onClick={() => getLastResult()}>ANS</Button>
                 </Rows>
                 <Rows>
                     <Button onClick={() => showValue('𝜋')}>𝜋</Button>
@@ -93,13 +159,13 @@ export function Calculator() {
                     <Button onClick={() => showValue('(')}>(</Button>
                     <Button onClick={() => showValue(')')}>)</Button>
                     <Button onClick={() => showValue('%')}>%</Button>
-                    <Button onClick={() => showValue('÷')}>÷</Button>
+                    <Button onClick={() => showValue('/')}>÷</Button>
                 </Rows>
                 <Rows>
                     <Button onClick={() => showValue('7')}>7</Button>
                     <Button onClick={() => showValue('8')}>8</Button>
                     <Button onClick={() => showValue('9')}>9</Button>
-                    <Button onClick={() => showValue('x')}>x</Button>
+                    <Button onClick={() => showValue('*')}>x</Button>
                 </Rows>
                 <Rows>
                     <Button onClick={() => showValue('4')}>4</Button>
@@ -117,7 +183,7 @@ export function Calculator() {
                     <Button onClick={() => showValue('convert')}>+/-</Button>
                     <Button onClick={() => showValue('0')}>0</Button>
                     <Button onClick={() => showValue(',')}>,</Button>
-                    <Button onClick={() => showValue('=')}>=</Button>
+                    <Button onClick={() => calc()}>=</Button>
                 </Rows>
 
             </Container>
