c1d13f4b4a79c19d7a1641dea94d93358396b50e
alex-arraga <alex-arraga@users.noreply.github.com>
2024-01-25 00:41:01 -0300

    Modify home design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,26 +22,49 @@ function HomePage() {
       <div className="bg-[url('/bg-main-blur-30.png')] flex justify-center items-center w-full">
         <main className="flex flex-col h-full max-w-[95vw] md:max-w-[90vw] mt-12 md:mt-14 xl:mt-16">
 
-          {/* About me */}
+          {/* About me, Social Networks */}
           <ContainerBoxes className="flex flex-col-reverse sm:flex-row my-2 sm:my-4 md:my-6 relative gap-2 md:gap-4 w-full h-full ">
             <Box className="box-me relative w-full sm:w-[calc(80%)] rounded-2xl">
               <AboutMe />
             </Box>
 
-            {/* Social Networks */}
             <Box className="flex flex-row sm:flex-col relative justify-between items-center w-full h-20 sm:h-auto sm:w-[calc(20%)] gap-2">
               <SocialNetworks />
             </Box>
           </ContainerBoxes>
 
-          {/* Skills */}
-          <ContainerBoxes className="flex flex-col md:flex-row relative gap-2 md:gap-4 w-full mb-2 md:mb-4 xl:mb-6">
+
+          {/* My Projects */}
+          <ContainerBoxes>
+            <Box className="w-full h-full rounded-2xl box-proyects p-4 md:p-6 xl:p-10 mb-2 md:mb-4 xl:mb-6">
+              <Projects />
+            </Box>
+          </ContainerBoxes>
+
+
+          {/* My age, English Level, Certicates */}
+          <ContainerBoxes className="flex w-full h-full gap-2 md:gap-4 mb-2 md:mb-4 xl:mb-6">
+            <Box className="box-english select-none w-1/2 relative flex flex-col gap-6 justify-center items-center p-4 md:p-6 xl:p-10 rounded-2xl">
+              <MyEnglish />
+            </Box>
+
+            <Box className="box-age select-none w-1/2 relative flex flex-col gap-6 justify-center items-center p-4 md:p-6 xl:p-10 rounded-2xl">
+              <MyAge />
+            </Box>
+
+            <Box className="relative border-2 border-violet-300 bg-red-200 w-1/2 rounded-2xl">
+              <Certificates />
+            </Box>
+          </ContainerBoxes>
+
+
+          {/* Work Experience, Personality, Skills */}
+          <ContainerBoxes className="flex flex-col md:flex-row relative gap-2 md:gap-4 w-full mb-10">
             <Box className="box-skills w-full md:w-1/2 p-4 md:p-6 xl:p-10 rounded-2xl">
               <Skills />
             </Box>
 
             <div className="flex flex-wrap gap-2 md:gap-4 w-full md:w-1/2">
-              {/* Work Experience */}
               <Box className="box-work_experience w-full relative h-[63%] p-4 md:p-6 xl:p-10 rounded-2xl">
                 <WorkExperience />
               </Box>
@@ -52,28 +75,7 @@ function HomePage() {
             </div>
           </ContainerBoxes>
 
-          {/* My age */}
-          <ContainerBoxes className="flex w-full h-full gap-2 md:gap-4 mb-2 md:mb-4 xl:mb-6">
-            <Box className="box-english select-none w-1/2 relative flex flex-col gap-6 justify-center items-center p-4 md:p-6 xl:p-10 rounded-2xl">
-              <MyEnglish />
-            </Box>
 
-            <Box className="box-age select-none w-1/2 relative flex flex-col gap-6 justify-center items-center p-4 md:p-6 xl:p-10 rounded-2xl">
-              <MyAge />
-            </Box>
-
-            {/* Certificates */}
-            <Box className="relative border-2 border-violet-300 bg-red-200 w-1/2 rounded-2xl">
-              <Certificates />
-            </Box>
-          </ContainerBoxes>
-
-          {/* My Projects */}
-          <ContainerBoxes>
-            <Box className="w-full h-full mb-10 rounded-2xl box-proyects p-4 md:p-6 xl:p-10">
-              <Projects />
-            </Box>
-          </ContainerBoxes>
         </main>
       </div>
     </div>
--- src/components/home-components/WorkExperience.tsx
+++ src/components/home-components/WorkExperience.tsx
@@ -17,32 +17,29 @@ function WorkExperience() {
             </div>
             <ul className="flex flex-col gap-6 text-xs md:text-sm xl:text-base text-gray-300">
                 <li>
-                    <span className="font-semibold text-gray-100">1 año - Freelance, Diseñador UX/UI.</span>
-                    <span className="leading-5 text-xs xl:text-sm">
-                        <br />
+                    <h3 className="font-semibold text-gray-100">1 año - Freelance, Diseñador UX/UI | <span className="text-orange-200">2023</span></h3>
+                    <p className="leading-5 text-xs xl:text-sm">
                         <br /> - Research y comprensión de las necesidades de los usuarios.
                         <br /> - Desarrollo de interfaces utilizando Figma, Whimsical y Adobe Photoshop.
                         <br /> - Proyecto de redesign en coparticipación con Wordpress Dev. para Hotel Yacanto - Cordoba
-                    </span>
+                    </p>
                 </li>
                 <li>
-                    <span className="font-semibold text-gray-100">4 años - Vendedor de Automoviles</span>
-                    <span className="leading-5 text-xs xl:text-sm">
-                        <br />
+                    <h3 className="font-semibold text-gray-100">4 años - Vendedor de Automoviles | <span className="text-orange-200">2018 - 2022</span></h3>
+                    <p className="leading-5 text-xs xl:text-sm">
                         <br /> - Venta de automóviles usados y 0km.
                         <br /> - Asesoramiento a clientes sobre las características y beneficios de los vehículos.
                         <br /> - Negociación de precios y condiciones de venta.
                         <br /> - Tramitación de créditos prendarios.
-                    </span>
+                    </p>
                 </li>
                 <li>
-                    <span className="font-semibold text-gray-100">1 año - Atención comercial en Perfumería</span>
-                    <span className="leading-5 text-xs xl:text-sm">
-                        <br />
+                    <h3 className="font-semibold text-gray-100">1 año - Atención comercial en Perfumería | <span className="text-orange-200">2017</span></h3>
+                    <p className="leading-5 text-xs xl:text-sm">
                         <br /> - Venta y asesoramiento sobre productos de la tienda.
                         <br /> - Manejo de caja y facturación cuando los encargados se ausentaban.
                         <br /> - Reposición de stock.
-                    </span>
+                    </p>
                 </li>
             </ul>
         </section>
