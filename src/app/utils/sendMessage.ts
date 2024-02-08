e6f7d55b7b0f0507aeb97f9c74d1f03e023e4b66
alex-arraga <alex-arraga@users.noreply.github.com>
2024-02-07 22:02:52 -0300

    ...

--- src/app/utils/sendMessage.ts
+++ src/app/utils/sendMessage.ts
@@ -1,5 +1,8 @@
 export const sendMessage = async (text: string) => {
     await fetch("https://api.telegram.org/bot6736589583:AAF0KAqA5pcNm_X9hc7AlSK1j06TZAyieEU/sendMessage", {
+        headers: {
+            "Content-Type": "application/json"
+        },
         method: "POST",
         body: JSON.stringify({
             chat_id: 6144907154,
