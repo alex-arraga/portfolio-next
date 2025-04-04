9828b21777efec1f61cf0b48afbf77925aa3fe39
alex-arraga <alex-arraga@users.noreply.github.com>
2025-04-04 17:37:33 -0300

    fixing size of calculator screen

--- src/components/calculator-components/Screen.tsx
+++ src/components/calculator-components/Screen.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export function Screen({ value }: { value: string }) {
     return (
         <form className='flex justify-center items-center mx-1 sm:mx-2'>
-            <input className='flex justify-end items-center px-3 h-14 md:h-16 rounded-md mt-3 mb-3 md:mb-5 overflow-auto bg-black bg-opacity-50 text-right text-xl sm:text-txt_24 md:text-txt_30 border-white border-2 border-opacity-30'
+            <input className='flex justify-end items-center max-w-[245px] sm:max-w-xs px-3 h-14 md:h-16 rounded-md mt-3 mb-3 md:mb-5 overflow-auto bg-black bg-opacity-50 text-right text-xl sm:text-txt_24 md:text-txt_30 border-white border-2 border-opacity-30'
                 placeholder='0'
                 type='text'
                 readOnly={true}
