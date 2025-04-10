ee73fd6f8a644d278dd4bcaf4337073f6f8acdf9
alex-arraga <alex-arraga@users.noreply.github.com>
2025-04-10 20:15:25 -0300

    sizes of my image in main page improved

--- src/components/home-components/AboutMe.tsx
+++ src/components/home-components/AboutMe.tsx
@@ -9,7 +9,7 @@ function AboutMe() {
     const userName = user.user?.username;
 
     return (
-        <div className="flex justify-end w-full h-full mb-2 md:mb-6 xl:mb-10">
+        <section className="flex justify-between w-full h-full mb-2 md:mb-6 xl:mb-10">
             <div className="w-full md:w-2/3">
                 <h1 className="mb-6 md:mb-8 xl:mb-10 font-bold text-xl md:text-3xl xl:text-4xl text-gray-100" > Hola <span className="text-orange-200 capitalize">{fullNameUser ? fullNameUser : userName ? userName?.replace(/[0-9-_]+/g, ' ') : 'internauta'}</span>, <br /> soy Alex! 😁</h1>
                 <p className="leading-6 text-sm md:text-base text-gray-300">
@@ -17,7 +17,7 @@ function AboutMe() {
                         Soy Diseñador UX/UI y Fullstack Developer
                     </span>
                     <span className="">
-                        , con una fuerte orientación al backend, actualmente estoy especializándome en el desarrollo de aplicaciones robustas y escalables usando Go  
+                        , con una fuerte orientación al backend, actualmente estoy especializándome en el desarrollo de aplicaciones robustas y escalables usando Go
                     </span>
                     <span>
                         . Durante un tiempo trabajé como diseñador UX/UI, lo que me dio una fuerte base en diseño centrado en el usuario. Además, mi paso por atención al cliente me enseñó la importancia de la empatía y la resolución de problemas en equipo.
@@ -25,17 +25,16 @@ function AboutMe() {
                 </p>
             </div>
 
-            <div className="relative hidden md:block md:w-1/3 h-full">
+            <div className="absolute hidden xl:block w-full max-w-xs h-full right-0 top-0">
                 <Image
                     src='/me.png'
                     alt="me"
                     fill
-                    sizes="1000px"
                     priority
                     className="object-cover rounded-2xl hidden md:block"
                 />
             </div>
-        </div>
+        </section>
     )
 }
 
