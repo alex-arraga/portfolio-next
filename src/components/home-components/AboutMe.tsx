3969c9e769b003e62e2d0389f38104daf8770e32
alex-arraga <alex-arraga@users.noreply.github.com>
2025-04-12 19:41:47 -0300

    main image removed

--- src/components/home-components/AboutMe.tsx
+++ src/components/home-components/AboutMe.tsx
@@ -10,7 +10,7 @@ function AboutMe() {
 
     return (
         <section className="flex justify-between w-full h-full mb-2 md:mb-6 xl:mb-10">
-            <div className="w-full md:w-2/3">
+            <div className="w-full">
                 <h1 className="mb-6 md:mb-8 xl:mb-10 font-bold text-xl md:text-3xl xl:text-4xl text-gray-100" > Hola <span className="text-orange-200 capitalize">{fullNameUser ? fullNameUser : userName ? userName?.replace(/[0-9-_]+/g, ' ') : 'internauta'}</span>, <br /> soy Alex! 😁</h1>
                 <p className="leading-6 text-sm md:text-base text-gray-300">
                     <span className="font-semibold text-white">
@@ -25,7 +25,7 @@ function AboutMe() {
                 </p>
             </div>
 
-            <div className="absolute hidden xl:block w-full max-w-xs h-full right-0 top-0">
+            {/* <div className="absolute hidden xl:block w-full max-w-xs h-full right-0 top-0">
                 <Image
                     src='/me.png'
                     alt="me"
@@ -33,7 +33,7 @@ function AboutMe() {
                     priority
                     className="object-cover rounded-2xl hidden md:block"
                 />
-            </div>
+            </div> */}
         </section>
     )
 }
