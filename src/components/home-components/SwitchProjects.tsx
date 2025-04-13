ed562235f817d3010a2c4e35877883f34a60337a
alex-arraga <alex-arraga@users.noreply.github.com>
2025-04-13 17:53:54 -0300

    fixing project counter

--- src/components/home-components/SwitchProjects.tsx
+++ src/components/home-components/SwitchProjects.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image"
 import { useHomeContext } from "@/context/HomeContext"
 import { useRouter } from "next/navigation"
+import { personalDesignProjects, personalCodeProjects } from "@/constants"
 
 function SwitchProjects({ type }: { type?: string }) {
     const { codeProjects, setCodeProjects } = useHomeContext()
@@ -38,6 +39,7 @@ function SwitchProjects({ type }: { type?: string }) {
 
                     :
 
+                    // Projects counter
                     <div className='relative box-switch rounded-2xl mb-6 md:mb-8 xl:mb-10 h-full p-4 md:p-6 xl:p-10 px-8 md:px-10 xl:px-20'>
                         <div className='flex items-center justify-center gap-2 md:gap-4 xl:gap-6 mb-4 md:mb-6'>
                             <Image
@@ -48,9 +50,13 @@ function SwitchProjects({ type }: { type?: string }) {
                                 height={50}
                                 className='object-contain hover:scale-110 duration-300 cursor-pointer w-[40px] h-[20px] md:w-[80px] md:h-[40px] xl:w-[120px] xl:h-[50px]'
                             />
-                            <h1 className='text-txt_30 md:text-txt_60 xl:text-txt_100 font-medium text-white'>Proyectos</h1>
+                            <h1 className='text-txt_30 md:text-txt_60 xl:text-txt_100 font-medium text-white'>
+                                Proyectos
+                            </h1>
                             <div className='flex justify-center items-center rounded-xl xl:rounded-2xl w-10 h-10 md:w-20 md:h-20 xl:w-28 xl:h-28 bg-violet-300'>
-                                <p className='text-txt_30 md:text-txt_60 xl:text-txt_80 text-slate-800 font-semibold'>{!codeProjects ? 4 : 3}</p>
+                                <p className='text-txt_30 md:text-txt_60 xl:text-txt_80 text-slate-800 font-semibold'>
+                                    {codeProjects ? personalCodeProjects.length : personalDesignProjects.length}
+                                </p>
                             </div>
                         </div>
 
